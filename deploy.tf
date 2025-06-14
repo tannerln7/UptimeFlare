d38d8d6623e79f2e94c4ee70363759454cb957d7
Tanner Lane <51337582+tannerln7@users.noreply.github.com>
2025-06-14 02:46:42 -0400

    Update deploy.tf

--- deploy.tf
+++ deploy.tf
@@ -1,13 +1,3 @@
-variable "PLEX_URL"      { type = string }
-variable "OVERSEERR_URL" { type = string }
-variable "RADARR_URL"    { type = string }
-variable "SONARR_URL"    { type = string }
-variable "NZBGET_URL"    { type = string }
-variable "UN_SERVER_URL" { type = string }
-variable "CLOUDFLARE_URL"{ type = string }
-variable "HA_URL"        { type = string }
-variable "UPTIME_KEY"    { type = string }
-
 terraform {
   required_providers {
     cloudflare = {
@@ -18,13 +8,21 @@ terraform {
 }
 
 provider "cloudflare" {
-  # read token from $CLOUDFLARE_API_TOKEN
+  # token is read from $CLOUDFLARE_API_TOKEN
 }
 
-variable "CLOUDFLARE_ACCOUNT_ID" {
-  # read account id from $TF_VAR_CLOUDFLARE_ACCOUNT_ID
-  type = string
-}
+variable "CLOUDFLARE_ACCOUNT_ID" { type = string }
+
+# Define one variable per secret
+variable "PLEX_URL"      { type = string }
+variable "OVERSEERR_URL" { type = string }
+variable "RADARR_URL"    { type = string }
+variable "SONARR_URL"    { type = string }
+variable "NZBGET_URL"    { type = string }
+variable "UN_SERVER_URL" { type = string }
+variable "CLOUDFLARE_URL"{ type = string }
+variable "HA_URL"        { type = string }
+variable "UPTIME_KEY"    { type = string }
 
 resource "cloudflare_workers_kv_namespace" "uptimeflare_kv" {
   account_id = var.CLOUDFLARE_ACCOUNT_ID
@@ -42,7 +40,11 @@ resource "cloudflare_worker_script" "uptimeflare" {
     name         = "UPTIMEFLARE_STATE"
     namespace_id = cloudflare_workers_kv_namespace.uptimeflare_kv.id
   }
-	vars = {
+}
+
+# Inject your secrets properly
+resource "cloudflare_worker_secret" "env" {
+  for_each    = {
     PLEX        = var.PLEX_URL
     OVERSEERR   = var.OVERSEERR_URL
     RADARR      = var.RADARR_URL
@@ -53,14 +55,16 @@ resource "cloudflare_worker_script" "uptimeflare" {
     HA          = var.HA_URL
     UPTIME_KEY  = var.UPTIME_KEY
   }
+  account_id  = var.CLOUDFLARE_ACCOUNT_ID
+  script_name = cloudflare_worker_script.uptimeflare.name
+  name        = each.key
+  secret_text = each.value
 }
 
 resource "cloudflare_worker_cron_trigger" "uptimeflare_worker_cron" {
   account_id  = var.CLOUDFLARE_ACCOUNT_ID
   script_name = cloudflare_worker_script.uptimeflare.name
-  schedules = [
-    "* * * * *", # every 1 minute, you can reduce the KV write by increase the worker settings of `kvWriteCooldownMinutes`
-  ]
+  schedules   = ["* * * * *"]
 }
 
 resource "cloudflare_pages_project" "uptimeflare" {
